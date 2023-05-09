6b413ece747a9714ae0887787b0ee75440f1b705
tkulagina <tkulagina1974@gmail.com>
2023-05-09 19:50:01 +0300

    install notiflix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+console.log(Notiflix);
+
 
 const DEBOUNCE_DELAY = 300;
+
+fetch(`https://restcountries.com/v3.1/name/eesti`)
+.then(response =>{
+    //console.log(response.json());
+    return response.json();
+})
+.then(country =>{
+    console.log(country);
+})
+.catch (error =>{
+    console.log(error);
+})
